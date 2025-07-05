00164ae980d78a528330d2d3b9a69ab045a4e181
unknown <hamidovtimur123@gmail.com>
2025-07-05 23:56:19 +0500

    Deploy to gh-pages v2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Learn-Front/', // Важно! Имя репозитория
+  base: '/WebLearn/',
   plugins: [react()],
-  build: {
-    outDir: 'docs' // Специально для GitHub Pages
-  }
 })
